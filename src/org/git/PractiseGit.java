f0b8c27cfa7d74cd98730adf00d02aa4745dde32
Razul27 <mohamedrazul916@gmail.com>
2022-07-23 08:00:34 +0530

    UPDATE

--- src/org/git/PractiseGit.java
+++ src/org/git/PractiseGit.java
@@ -3,6 +3,11 @@ package org.git;
 public class PractiseGit {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
