ed2591e5ff29e58d73240204cc1f0c5e33078566
Razul27 <mohamedrazul916@gmail.com>
2022-07-23 07:52:46 +0530

    New Code

---
+++ src/org/git/PractiseGit2.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class PractiseGit2 {
+	public static void main(String[] args) {
+	int a[]=new int [5];
+	a[0]=10;
+	a[1]=10;
+	a[1]=10;
+for (int i : a) {
+	System.out.println(i);
+	
+}
+	}
+}
